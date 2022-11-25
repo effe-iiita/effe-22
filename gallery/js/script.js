8c5b091a4f775456bbea72c6e8a7ea533852992a
SK-ILLish-GIT <sksahilparvez2000@gmail.com>
2022-11-25 17:06:13 +0530

    sponsor and gallery updated

--- gallery/js/script.js
+++ gallery/js/script.js
@@ -4,56 +4,92 @@ const data = [
         title: "Neeti Mohan",
     },
     {
-        src: "image/denis.jpg",
-        title: "Neeti Mohan",
+        src: "image/favourite_movie_revealed.jpg",
+        title: "BANDS",
     },
     {
-        src: "image/denis.jpg",
-        title: "Neeti Mohan",
+        src: "image/dreamy2.jpg",
+        title: "dancing",
     },
     {
-        src: "image/denis.jpg",
-        title: "Neeti Mohan",
+        src: "image/sleigher3000.jpg",
+        title: "akash gupta",
     },
     {
-        src: "image/denis.jpg",
-        title: "Neeti Mohan",
+        src: "image/fighter3.jpg",
+        title: "motivation",
     },
     {
-        src: "image/denis.jpg",
-        title: "Neeti Mohan",
+        src: "image/gumble.jpg",
+        title: "gumble",
     },
     {
-        src: "image/denis.jpg",
-        title: "Neeti Mohan",
+        src: "image/fighter1.jpg",
+        title: "Bassi",
     },
     {
-        src: "image/denis.jpg",
-        title: "Neeti Mohan",
+        src: "image/confettis.jpg",
+        title: "FACE ART",
     },
     {
-        src: "image/denis.jpg",
-        title: "Neeti Mohan",
+        src: "image/dangermouse.jpg",
+        title: "dangermouse",
     },
     {
-        src: "image/denis.jpg",
-        title: "Neeti Mohan",
+        src: "image/cooking.jpg",
+        title: "cooking",
     },
     {
-        src: "image/denis.jpg",
-        title: "Neeti Mohan",
+        src: "image/dreamy1.jpg",
+        title: "dreamy",
     },
     {
-        src: "image/denis.jpg",
-        title: "Neeti Mohan",
+        src: "image/spotbots_switched_on.jpg",
+        title: "spotbots",
     },
     {
-        src: "image/denis.jpg",
-        title: "Neeti Mohan",
+        src: "image/plane_to_sphere.jpg",
+        title: "singing",
     },
     {
-        src: "image/denis.jpg",
-        title: "Neeti Mohan",
+        src: "image/sherrifcali.jpg",
+        title: "sherrifcali",
+    },
+    {
+        src: "image/giugiu.jpg",
+        title: "Farhan aktar",
+    },
+    {
+        src: "image/handsup_001.jpg",
+        title: "handsup",
+    },
+    {
+        src: "image/icon_rugby.jpg",
+        title: "freshee",
+    },
+    {
+        src: "image/icon_saxo.jpg",
+        title: "Bike skill",
+    },
+    {
+        src: "image/address_revealed.jpg",
+        title: "DJ",
+    },
+    {
+        src: "image/blob_001.jpg",
+        title: "dramming",
+    },
+    {
+        src: "image/icon_tinder.jpg",
+        title: "speech",
+    },
+    {
+        src: "image/icon_twitter.jpg",
+        title: "Art",
+    },
+    {
+        src: "image/IMG_7092.jpg",
+        title: "dramming",
     },
 ];
 const container = document.querySelector(".img-container");
